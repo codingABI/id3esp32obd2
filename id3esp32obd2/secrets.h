925470bc5aacf9908b106b6110afa27afdb1d997
codingABI <50620368+codingABI@users.noreply.github.com>
2023-06-24 20:05:22 +0200

    Create secrets.h

---
+++ id3esp32obd2/secrets.h
@@ -0,0 +1,4 @@
+// File for data which should not be published to the public
+
+// MAC address of your mobile phone. Only this mobile phone is allowed to receive data
+const char g_macMobilePhone[6] = {0x01,0x02,0x03,0x04,0x05,0x06};
