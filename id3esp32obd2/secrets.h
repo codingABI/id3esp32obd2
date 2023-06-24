48dc5460f8905cfc3eabf9314b6e419fb63286c7
codingABI <50620368+codingABI@users.noreply.github.com>
2023-06-24 23:02:35 +0200

    Update secrets.h

--- id3esp32obd2/secrets.h
+++ id3esp32obd2/secrets.h
@@ -1,4 +1,5 @@
 // File for data which should not be published to the public
 
 // MAC address of your mobile phone. Only this mobile phone is allowed to receive data
+// You see the connecting MAC address by serial output from esp32 "Bluetooth client connected with address:..." 
 const char g_macMobilePhone[6] = {0x01,0x02,0x03,0x04,0x05,0x06};
