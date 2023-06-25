2091b71f509cef99c3a273514be006a941f1631a
codingABI <50620368+codingABI@users.noreply.github.com>
2023-06-25 19:12:17 +0200

    Update secrets.h

--- id3esp32obd2/secrets.h
+++ id3esp32obd2/secrets.h
@@ -1,6 +1,6 @@
 // File for data which should not be published to the public
 
-/* MAC address of your mobile phone. Only this mobile phone is allowed to receive data
+/* MAC address of your android device. Only this device is allowed to receive data
  * You can see the connecting MAC address by serial output from ESP32 "Bluetooth client connected with address:..."
  */
-const char g_macMobilePhone[6] = {0x01,0x02,0x03,0x04,0x05,0x06};
+const char g_macMobileDevice[6] = {0x01,0x02,0x03,0x04,0x05,0x06};
