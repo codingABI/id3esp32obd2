7feb60f96605dbf24e25831b3f27d5d1ffee0e18
codingABI <50620368+codingABI@users.noreply.github.com>
2023-06-26 13:19:33 +0200

    Update dashboards.ino

--- id3esp32obd2/dashboards.ino
+++ id3esp32obd2/dashboards.ino
@@ -124,29 +124,3 @@ void requestMyDashboard(byte boardID) {
   if (boardID != g_requestDashboard) return; // Bluetooth client changed dashboard
   readAndSendHVTotalChargeDischarge();
 }
-
-// Only for testing
-void requestDashboardTest() {
-  #define MAXSTRDATALENGTH 80
-  char strData[MAXSTRDATALENGTH+1];
-
-  checkBTReceive();
-  if (sendUDSRequest(0x7A0,UDS_ReadDataByIdentifier_0x22,0xC0,0x0B)) {
-    /* Speed
-     * 
-     * Example:
-     * 22:38:34.646 -> Send    ID: 17FC007B Flags:1 Length:8 Data:03 22 F4 0D 00 00 00 00 
-     * 22:38:34.646 -> Receive ID: 17FE007B Flags:1 Length:8 Data:04 62 F4 0D 00 AA AA AA 
-     */
-    Serial.print("Test : ");
-   // Serial.println(g_dataBuffer[0]);
-    Serial.println(((g_dataBuffer[8]<<24)+(g_dataBuffer[9]<<16)+(g_dataBuffer[10]<<8)+g_dataBuffer[11])/8583.07123641215);
-    Serial.println(((g_dataBuffer[12]<<24)+(g_dataBuffer[13]<<16)+(g_dataBuffer[14]<<8)+g_dataBuffer[15])/8583.07123641215);
-
-    snprintf(strData,MAXSTRDATALENGTH+1,"%i|%i",idSPEED,(g_dataBuffer[0]<<8) +g_dataBuffer[1]);
-    g_SerialBT.write((byte*)strData,strlen(strData)+1); 
-  } else {
-    snprintf(strData,MAXSTRDATALENGTH+1,"%i|ERR",idSPEED);
-    g_SerialBT.write((byte*)strData,strlen(strData)+1);    
-  }
-}
